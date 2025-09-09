e47c1b7a63014424e6f56761d6740458bbb26cdb
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-09 16:07:38 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,25 @@
+# \n - new/next line
+# \t -tab
+# \\-backslash
+#\' - inserts a single quote inside a single-quoted string
+
+#\n
+#print("hello everyone.\nhow are you?")
+
+# \t
+#print("john 20")
+#print("john\t20")
+
+#\\
+#print("new\\old")
+#print("yes\\no")
+
+# \'
+#print('this is python\'s class')
+#print("this is a python's class")
+
+# \"
+print("he says, \"we are learning python\"")
+
+
+
