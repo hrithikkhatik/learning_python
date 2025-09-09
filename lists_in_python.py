4d46f1d95980c89d442cc38f99c332c01c7472e5
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-09 20:20:34 +0530

    Add files via upload

---
+++ lists_in_python.py
@@ -0,0 +1,18 @@
+name="john"
+age=20
+percent=85.5
+
+student=["john",20,85.5]
+print(type(student))
+print(student)
+
+day_of_week = ["mon","tue","wed","thur","fri","sat","sun"]
+print(day_of_week[0])
+print(day_of_week[4])
+print(f"last day of week is : {day_of_week[-1]}")
+
+#length of a list => the number of items/elements in the list
+print(len(day_of_week))
+print(day_of_week[8])
+
+
