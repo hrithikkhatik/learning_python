6111564e6ef778a913037dc596aa56f7c6b6e4a2
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-09 15:47:51 +0530

    Add files via upload

---
+++ fstring_python.py
@@ -0,0 +1,17 @@
+name = "john"
+age = 20
+language="python"
+hours = 3
+
+#john is 20 years old. he studies python 3 hours a day.
+print(name,"is",age,"years old. he studies",language , hours,"hours a day" )
+#using f-string
+print(f"{name} is {age} years old. he studies {language} {hours} hours a day")
+
+sub1=78
+sub2=87
+sub3=83
+#print(f"{name} scored {sub1+sub2+sub3} marks in total.")
+percent=(sub1+sub2+sub3) / 3
+print(f"{name} got {percent}%")
+
