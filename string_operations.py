b02f177faad5606d221babfdd2bb8875fa0b17fd
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-09 19:23:34 +0530

    Add files via upload

---
+++ string_operations.py
@@ -0,0 +1,58 @@
+"""s1="python is fun"
+print(s1[0])
+print(s1[-1])
+print(len(s1))
+
+language="python"
+version="3.13.3"
+print(language + version)
+print("python" - "p")
+print("python" * "p")
+s1="python"
+print(s1 * 3)
+
+#membership operation
+# in
+s1="python is fun"
+print("python" in s1)
+print("i" in s1)
+print("z" in s1)
+print("java" in s1)
+
+#not in
+s1="python is fun"
+print("z" not in s1)
+print("java" not in s1)
+print("python" not in s1)
+
+
+# comparison of string
+print("python" =="python")
+print("python " =="python")
+
+#removing spaces from a string - strip()
+
+s1= "python "
+s2 = (s1.strip()  == "python")
+print(s2)
+
+s3= "           python            "
+s4 = (s1.strip()  == "python")
+print(s2)
+"""
+# replace()
+s1 = "we are learning python"
+print(s1)
+print(s1.replace("python","java"))
+print(s1)
+print(s1.replace("e","E"))
+print(s1.replace("e","E",1))
+
+
+
+
+
+
+
+
+
