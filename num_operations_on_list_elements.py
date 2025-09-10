d12b2e796ae8c3675191b265aac804dc99ba10b4
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-10 12:50:57 +0530

    Add files via upload

---
+++ num_operations_on_list_elements.py
@@ -0,0 +1,28 @@
+numbers= [10,4,5.5,7,1]
+
+#smmallest number in the list
+#min()
+print(min(numbers))
+print(f"smallest number is:{min(numbers)}")
+
+#biggest number in list
+#max()
+print(f"biggest number is: {max(numbers)}")
+
+numbers= [10,4,-1,20,5.5,7,1]
+print(f"smallest number is:{min(numbers)}")
+print(f"biggest number is: {max(numbers)}")
+
+#numbers= [10,4,-1,20,5.5,7,1,"hi"]
+print(f"smallest number is:{min(numbers)}")
+print(f"biggest number is: {max(numbers)}")
+
+#total of the numbers in list
+#sum()
+numbers= [10,4,-1,20,5.5,7,1]
+print(f"total of numbers is: {sum(numbers)}")
+
+
+
+
+
