f14d92776171c7604a610b41beefcd8d278a0965
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-10 13:04:39 +0530

    Add files via upload

---
+++ nested_lists.py
@@ -0,0 +1,13 @@
+#list inside a list
+l1=[5,1.5,"python",True,None,[1,2,3],10]
+print(len(l1))
+print(l1[-2])
+print(l1[-2][0])
+
+l2 = [[1,2],[3,4],[5,6,[0,1]]]
+print(len(l2))
+print(l2[-1])
+print(l2[-1][-1])
+print(l2[-1][-1][-1])
+
+
