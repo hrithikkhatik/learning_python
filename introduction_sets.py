1ddfd418988d52138e417918241a5359a76a7626
hrithikkhatik <hrithikkhatik360@gmail.com>
2025-09-10 20:16:34 +0530

    Add files via upload

---
+++ introduction_sets.py
@@ -0,0 +1,18 @@
+#sets are non-sequential collection of items
+# comma separated elements enclosed within {}
+
+set1={10,"python",2.5}
+print(set1)
+print(type(set1))
+
+#cannot have indexing with sets
+#print(set1[0])
+
+#length of set
+print(len(set1))
+
+#sets do not allow duplicate elements
+l1=[10,2.5,10,30,10]
+print(l1,type(l1))
+s1={10,2.5,10,30,10}
+print(s1,type(s1))
